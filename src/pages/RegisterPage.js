2a110888c94f15edfeb7523a7247f0730422d312
rokas <rokasra@gmail.com>
2022-06-29 11:13:28 +0300

    register page

--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -1,7 +1,25 @@
 function RegisterPage() {
   return (
-    <div>
-      <h1>Register Page</h1>
+    <div className='container'>
+      <h1 className='display-4 py-4 text-center'>Register Here</h1>
+
+      <form className='jumbotron w-50 mx-auto'>
+        <div className='form-group'>
+          <label htmlFor='email'>Email</label>
+          <input type='email' className='form-control' id='email' />
+        </div>
+        <div className='form-group'>
+          <label htmlFor='password'>Password</label>
+          <input type='password' className='form-control' id='password' />
+        </div>
+        <div className='form-group'>
+          <label htmlFor='repeatPassword'>Repeat Password</label>
+          <input type='password' className='form-control' id='repeatPassword' />
+        </div>
+        <button type='submit' className='btn btn-outline-dark'>
+          Register
+        </button>
+      </form>
     </div>
   );
 }
